647b2672a435f73df53c9ebb5a96efa9d747e86e
Shiv Lahejat <shivlahejat123@gmail.com>
2022-05-23 20:48:32 +0530

    test upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import test from "../src/componets/test";
 function App() {
   return (
     <div className="App">
       <h1> Hello World </h1>
+      <test />
     </div>
   );
 }
